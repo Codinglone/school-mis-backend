4a21658416dac74d41a8be63e7d52e6e3ec4d07f
Fabrice <codinglone@gmail.com>
2023-01-14 12:23:29 +0200

    Added create user opts in schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,4 +1,5 @@
 import {Type as T} from "@sinclair/typebox";
+import { createUserController } from "../controllers/user.controller";
 
 const createUserSchema = T.Object({
     firstName: T.String(),
@@ -8,3 +9,25 @@ const createUserSchema = T.Object({
     role: T.String(),
     school: T.String()
 })
+
+const userSchema = T.Object({
+    id: T.Number(),
+    firstName: T.String(),
+    lastName: T.String(),
+    email: T.String(),
+    password: T.String(),
+    role: T.String(),
+    school: T.String(),
+    createdAt: T.String(),
+    updatedAt: T.String()
+})
+
+const createUserOpts = {
+    schema: {
+        body: T.Strict(createUserSchema),
+        response: {
+            200: userSchema
+        }
+    },
+    handler: createUserController
+}
