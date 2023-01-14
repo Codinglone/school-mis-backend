a96a3710fd5450d5e7b8cd6ece2b44810cbd36c9
Fabrice <codinglone@gmail.com>
2023-01-14 12:42:11 +0200

    Registered routes and swagger in index

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import { AppDataSource } from "./data-source"
-const fastify = require('fastify');
+const fastify = require('fastify')({logger: true});
+const PORT = parseInt(process.env.FASTIFY_PORT) || 5000;
 
+fastify.register(require('@fastify/swagger'), {
+    exposeRoute: true,
+    routePrefix: '/api-docs',
+    swagger: {
+        info: {
+            title: 'School MIS Backend',
+            description: 'School MIS Backend using TypeORM, Fastify and Postgres',
+            version: "1.0.0",
+        }
+    }
+})
+
+fastify.register(require('./routes/school.routes'))
 
 AppDataSource.initialize().then(async () => {
 
     const start = async () => {
         try {
-          await fastify.listen({ port: 3000 })
+          await fastify.listen({ port:  PORT})
         } catch (err) {
           fastify.log.error(err)
           process.exit(1)
