0f3e38bbc3876bdf34aa2a24a108eb749f2d6ed6
Fabrice <codinglone@gmail.com>
2023-01-14 12:36:13 +0200

    Initialized fastify and added fastify port in .env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+const fastify = require('fastify');
+
 
 AppDataSource.initialize().then(async () => {
 
-
+    const start = async () => {
+        try {
+          await fastify.listen({ port: 3000 })
+        } catch (err) {
+          fastify.log.error(err)
+          process.exit(1)
+        }
+      }
+      start()
     console.log("connected!!")
 
 }).catch(error => console.log(error))
--- src/routes/school.routes.ts
+++ src/routes/school.routes.ts
@@ -3,4 +3,7 @@ import { createUserOpts } from "../schemas/user.schema";
 
 const usersRoutes = (fastify, opts, done) => {
     fastify.post(userEndpoints.USERS, createUserOpts);
+    done()
 }
+
+module.exports = {usersRoutes}
