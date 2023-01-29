42f40ac37e4830ca52e55bc606bc69d132f9ede5
Fabrice <codinglone@gmail.com>
2023-01-29 19:03:22 +0200

    Added bcrypt to the password

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,10 @@
 import { UserModel } from "../models/users.model";
+const bcrypt = require('bcrypt');
 export const createUserController = async(req, reply) => {
     try {        
-        const response = await UserModel.postCreateUser(req.body);
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        req.body.password = hashedPassword;
+        const response = await UserModel.postCreateUser(req.body)
         reply.code(201).send(response);
     } catch (error) {
         reply.code(400).send(error)
@@ -20,6 +23,8 @@ export const getUsersController = async(req, reply) => {
 
 export const loginController = async(req, reply) => {
     try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        req.body.password = hashedPassword;
         const response = await UserModel.loginUser(req.body.email, req.body.password)
         reply.code(200).send(response)
     } catch (error) {
