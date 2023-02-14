03b407c275129c3dddfdf24d43d2dd41f8a006a2
Fabrice <codinglone@gmail.com>
2023-02-14 15:20:01 +0200

    Added save functionality to the create category model function

--- src/models/categories.model.ts
+++ src/models/categories.model.ts
@@ -2,8 +2,16 @@ import { AppDataSource } from "../data-source";
 import { Categories } from "../entity/Categories";
 import { categoryPayload } from "../utils/payloads";
 
+const categoriesRepo = AppDataSource.getRepository(Categories);
+
 export class CategoriesModel {
     static postCreateCategory = async (payload: categoryPayload): Promise<any> => {
-
+     
+            const category = new Categories()
+            return await categoriesRepo.save({
+                ...category,
+                ...payload
+            })
+     
     }
 }
