95156fa1c6785e55bd65c883f263fe597f6f5caa
Fabrice <codinglone@gmail.com>
2023-02-14 15:07:40 +0200

    Added categories table to data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
+import { Categories } from "./entity/Categories"
 require('dotenv').config()
 
 export const AppDataSource = new DataSource({
@@ -12,7 +13,7 @@ export const AppDataSource = new DataSource({
     database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [User, Categories],
     migrations: [],
     subscribers: [],
 })
