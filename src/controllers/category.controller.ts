d5815911a27cd7366e81fe1ccdb9d01e5040612c
Fabrice <codinglone@gmail.com>
2023-02-16 12:30:09 +0200

    Added try catch block to categories controller

---
+++ src/controllers/category.controller.ts
@@ -0,0 +1,11 @@
+import { CategoriesModel } from "../models/categories.model";
+
+export const createCategoryController = async(req, reply) => {
+    try {
+        const response = await CategoriesModel.postCreateCategory(req.body);
+        reply.code(201).send(response);
+        
+    } catch (error) {
+        reply.code(400).send(error)
+    }
+}
