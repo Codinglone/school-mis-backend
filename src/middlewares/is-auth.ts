5098da3bd5286fe1c5a6fee3569638b16d40c598
Fabrice <codinglone@gmail.com>
2023-04-28 10:00:59 +0200

    Added verify token functionality

--- src/middlewares/is-auth.ts
+++ src/middlewares/is-auth.ts
@@ -6,6 +6,18 @@ const jwt = require('jsonwebtoken');
 const checkauth = (req, res, next) => {
     try {
         const authHeader = req.headers.authorization
+        const token = authHeader.substring(7)
+        console.log("Received token ==> ", token);
+        if(!token){
+            return res.code(401).send({ message: "Access denied, Try again!" })
+        }
+        else {
+            const verifyToken = jwt.verify(
+                token,
+                process.env.AUTH_SECRET
+            )
+        }
+        
     } catch (error) {
         res.code(401).send({ message: "Wrong Authentication!", error })
     }
