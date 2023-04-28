0a52ef9c5bb40c9e7f88d5f415f51441ffa23fa3
Fabrice <codinglone@gmail.com>
2023-04-28 09:53:27 +0200

    Created checkauth function

--- src/middlewares/is-auth.ts
+++ src/middlewares/is-auth.ts
@@ -1,2 +1,9 @@
+import { AppDataSource } from "../data-source";
+import { User } from "../entity/User";
+
+const checkauth = () => {
+    
+}
+
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt')
