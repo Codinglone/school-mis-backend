bc61b86a1a87561e5128ed749f0e6b057637ab98
Fabrice <codinglone@gmail.com>
2023-04-30 15:52:07 +0200

    Changes

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -2,7 +2,7 @@ const multer = require("fastify-multer")
 
 const storage = multer.diskStorage({
     destination: (req: any, file: any, cb: any) => {
-        cb(null, "./uploads")
+        cb(null, "uploads/")
     },
     filename: (req, file, cb) => {
         cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname)
