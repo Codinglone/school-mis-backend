2267627433a2f4d2836c224276b5b6fccf8685f7
Fabrice <codinglone@gmail.com>
2023-04-30 14:33:10 +0200

    Added preHandler to courseOpts

---
+++ src/schemas/course.schema.ts
@@ -0,0 +1,19 @@
+import { Type as T } from "@sinclair/typebox";
+import upload from "../utils/images";
+import { createCourseController } from "../controllers/course.controller";
+
+const courseSchema = T.Object({
+    name: T.String(),
+    profile: T.String()
+})
+
+const courseOpts = {
+    schema: {
+        // body: T.Strict(courseSchema)
+        response: {
+            201: courseSchema
+        }
+    },
+    preHandler: upload.fields([{ name: "profile" }]),
+    handler: createCourseController
+}
