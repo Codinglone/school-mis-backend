ee423647b38404e203459726d68fa6c32a907556
Fabrice <codinglone@gmail.com>
2023-04-30 14:15:16 +0200

    Finished postCreateCourse function

--- src/models/courses.model.ts
+++ src/models/courses.model.ts
@@ -6,6 +6,10 @@ const coursesRepo = AppDataSource.getRepository(Courses)
 
 export class CoursesModel {
     static postCreateCourse = async (payload: coursesPayload): Promise<any> => {
-
+        const course = new Courses()
+        return await coursesRepo.save({
+            ...payload,
+            ...course
+        })
     }
 }
