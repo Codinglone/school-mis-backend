c72be116a643f936cae0eaf1549c575c1e490d89
Fabrice <codinglone@gmail.com>
2023-04-30 14:40:02 +0200

    Added courses routes

--- src/routes/school.routes.ts
+++ src/routes/school.routes.ts
@@ -1,5 +1,5 @@
 import { createCategoryOpts, getCategoriesOpts } from './../schemas/category.schema';
-import { userEndpoints, categoryEndpoints } from "../utils/enums";
+import { userEndpoints, categoryEndpoints, courseEndpoints } from "../utils/enums";
 import { createUserOpts, getUsersOpts, loginOpts } from "../schemas/user.schema";
 import { courseOpts } from '../schemas/course.schema';
 
@@ -9,6 +9,7 @@ const usersRoutes = (fastify, opts, done) => {
     fastify.post(`${userEndpoints.USERS}/login`, loginOpts);
     fastify.post(categoryEndpoints.CATEGORIES, createCategoryOpts);
     fastify.get(categoryEndpoints.CATEGORIES, getCategoriesOpts)
+    fastify.post(courseEndpoints.COURSES, courseOpts)
     done()
 }
 
