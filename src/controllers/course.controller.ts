6c3c2b8291b117640dad8ebe81a85c851414e0f3
Fabrice <codinglone@gmail.com>
2023-04-30 14:26:19 +0200

    Added createCourseController function

---
+++ src/controllers/course.controller.ts
@@ -0,0 +1,10 @@
+import { CoursesModel } from "../models/courses.model";
+
+export const createCourseController = async (req, reply) => {
+    try {
+        const response = await CoursesModel.postCreateCourse(req.body, req.files.profile[0].key)
+        reply.code(201).send(response)
+    } catch (error) {
+        reply.code(204).send(error)
+    }
+}
