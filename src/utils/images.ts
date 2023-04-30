52ed12621ff8bea3ecfe6c851ba5885ed7a711ad
Fabrice <codinglone@gmail.com>
2023-04-30 13:48:10 +0200

    Created fileupload util

---
+++ src/utils/images.ts
@@ -0,0 +1,14 @@
+const multer = require("fastify-multer")
+
+const storage = multer.diskStorage({
+    destination: (req: any, file: any, cb: any) => {
+        cb(null, "./uploads")
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname)
+    } 
+})
+
+const upload = multer({ storage })
+
+export default upload;
