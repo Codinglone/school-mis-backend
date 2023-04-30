6876a80af7289caedbdd6e31c1f3fd41b3c68ebf
Fabrice <codinglone@gmail.com>
2023-04-30 14:17:47 +0200

    Added profile argument to postCreateCourse function

--- src/models/courses.model.ts
+++ src/models/courses.model.ts
@@ -5,7 +5,7 @@ import { coursesPayload } from "../utils/payloads";
 const coursesRepo = AppDataSource.getRepository(Courses)
 
 export class CoursesModel {
-    static postCreateCourse = async (payload: coursesPayload): Promise<any> => {
+    static postCreateCourse = async (payload: coursesPayload, profile:any): Promise<any> => {
         const course = new Courses()
         return await coursesRepo.save({
             ...payload,
